fcf877de63accc0cc18a9c637d8571c64f3ef068
Andrea <you@example.com>
2017-10-05 15:18:28 -0400

    home updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,17 +17,29 @@ const About = () =>
         <Col size="md-12">
           <h1>Welcome To SmartGrocer!</h1>
         </Col>
-      </Row>
       <Row>
         <Col size="md-12">
-          
+        </Col>
+      </Row>
+     <br/>
+      <Row>
+        <Col size="md-12">
+          <p className="text-center">
+            We all know going grocery shopping is a hassle. Even more so when you're squinting at the list you scribbled down on your way out the door! And it's all the same old recipes too. Well now SmartGrocer is here to help you break out of your rut without breaking the bank. 
+          </p>
+          <p className="text-center">
+           On our recipes page use the preferences icon to select what kind of recipes you would like to eat. SmartGrocer then provides you with a number of delicious suggestions. Select one and if you like what you see, click "Add Ingredients" to add them to your shopping list. Do this to your hert's content! When you've finished select the list tab to see all the ingredients you'll need grouped in one place! No scraps of paper or bad handwriting required.  
+          </p>
+          <p className="text-center">
+            Happy Shopping!
+          </p>
         </Col>
       </Row>
       <Row>
       <Col size="md-12">
       </Col>
       </Row>
-    </Container>
-  </div>;
+      </Container>
+  </div>
 
 export default About;
--- server.js
+++ server.js
@@ -3,19 +3,28 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
-const path = require("path")
+const PORT = process.env.PORT || 3001;
+//const path = require("path")
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static("client/build"));
+//if(process.env.NODE.ENV === 'production')
+app.use(express.static("client/public"));
 
-app.use(function(req, res, next) {
+
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(routes);
 
