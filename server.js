fbe987f43ecc43cb60a9eae7a57f241698284640
Andrea <you@example.com>
2017-10-06 14:01:21 -0400

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //if(process.env.NODE.ENV === 'production')
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 
 
 
